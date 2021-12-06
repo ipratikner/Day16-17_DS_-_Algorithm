8b0006eb7b58b81969179607d9f410e777931890
ipratikner <pratiknerlikar@gmail.com>
2021-12-06 19:19:44 +0530

    Added: used iterative method

---
+++ src/main/java/com/bridgelabz/IterativeMethod.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class IterativeMethod {
+    /**
+     * Iterative function to find permutations of a string
+     * using Collections
+     */
+    public static void permutations(String str) {
+        if (str == null || str.length() == 0) {
+            return;
+        }
+        List<String> partial = new ArrayList<>();
+        partial.add(String.valueOf(str.charAt(0)));
+        for (int i = 1; i < str.length(); i++) {
+            for (int j = partial.size() - 1; j >= 0; j--) {
+                String s = partial.remove(j);
+                for (int k = 0; k <= s.length(); k++) {
+                    partial.add(s.substring(0, k) + str.charAt(i) + s.substring(k));
+                }
+            }
+        }
+        System.out.println(partial);
+    }
+    public static void main(String[] args) {
+        String str = "ABCD";
+        permutations(str);
+    }
+}
